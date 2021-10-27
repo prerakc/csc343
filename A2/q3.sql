50e4304ad5d18b2e07f2b1b48860d00975a2f315
prerakc <prerak.chaudhari@yahoo.ca>
2021-10-26 23:13:00 -0400

    part1 question3 solution v1

--- A2/q3.sql
+++ A2/q3.sql
@@ -13,11 +13,51 @@ CREATE TABLE q3 (
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS ActualPurchases CASCADE;
+DROP VIEW IF EXISTS ExpectedPurchases CASCADE;
+DROP VIEW IF EXISTS DidNotPurchase CASCADE;
+DROP VIEW IF EXISTS PurchasedAll CASCADE;
+DROP VIEW IF EXISTS Reviewed CASCADE;
+DROP VIEW IF EXISTS NotReviewed CASCADE;
+DROP VIEW IF EXISTS ReviewedAll CASCADE;
 
 
 -- Define views for your intermediate steps here:
+CREATE VIEW ActualPurchases AS
+SELECT DISTINCT Purchase.CID, Item.IID, category
+FROM Purchase, LineItem, Item
+WHERE Purchase.PID = LineItem.PID AND LineItem.IID = Item.IID;
 
+CREATE VIEW ExpectedPurchases AS
+SELECT *
+FROM (SELECT CID FROM Customer) cmer CROSS JOIN (SELECT IID, category FROM Item) it;
+
+CREATE VIEW DidNotPurchase AS
+(SELECT * FROM ExpectedPurchases) EXCEPT (SELECT * FROM ActualPurchases);
+
+CREATE VIEW PurchasedAll AS
+SELECT *
+FROM ActualPurchases
+WHERE ROW(CID, category) NOT IN (SELECT DISTINCT CID, category FROM DidNotPurchase);
+
+CREATE VIEW Reviewed AS
+SELECT Review.CID, Review.IID, category
+FROM PurchasedAll, Review
+WHERE PurchasedAll.CID = Review.CID
+AND PurchasedAll.IID = Review.IID
+AND comment IS NOT NULL;
+
+CREATE VIEW NotReviewed AS
+(SELECT * FROM PurchasedAll) EXCEPT (SELECT * FROM Reviewed);
+
+CREATE VIEW ReviewedAll AS
+(SELECT DISTINCT cid, category FROM PurchasedAll)
+EXCEPT
+(SELECT DISTINCT cid, category from NotReviewed);
 
 -- Your query that answers the question goes below the "insert into" line:
 insert into q3
+(
+    SELECT *
+    FROM ReviewedAll
+);
--- A2/sample-data.sql
+++ A2/sample-data.sql
@@ -6,7 +6,8 @@ INSERT INTO Item VALUES
 (2, 'Book', 'A Thousand Splendid Suns', 14.00),
 (3, 'Book', 'Homegoing', 22.00),
 (4, 'Book', 'Trickster', 18.00),
-(5, 'Toy', 'Lego Hogwarts School of Witchcraft and Wizardry', 99.00);
+(5, 'Toy', 'Lego Hogwarts School of Witchcraft and Wizardry', 99.00),
+(6, 'Toy', 'AR-15', 999.99);
 
 -- Customer(CID, email, lastName, firstName, title)
 INSERT INTO Customer VALUES
@@ -19,7 +20,8 @@ INSERT INTO Customer VALUES
 INSERT INTO Purchase VALUES
 (100, 1515, '2019-11-01', 12345, 'Amex'),
 (101, 1500, '2019-11-01', 64210, 'Visa'),
-(102, 1518, '2021-01-01', 99999, 'Mastercard');
+(102, 1518, '2021-01-01', 99999, 'Mastercard'),
+(103, 1515, '2019-11-02', 12345, 'Amex');
 
 -- LineItem(PID, IID, quantity)
 INSERT INTO LineItem VALUES
@@ -27,12 +29,20 @@ INSERT INTO LineItem VALUES
 (100, 1, 2),
 (100, 5, 1),
 (101, 2, 4),
-(102, 3, 10);
+(102, 3, 10),
+(102, 1, 1),
+(102, 2, 1),
+(102, 4, 1),
+(103, 6, 1);
 
 -- Review(CID, IID, rating, comment)
 INSERT INTO Review VALUES
 (1515, 4, 5, 'Fantastic read!'),
-(1518, 4, 5, 'Ron said it was fantastic and he was right!!!');
+(1518, 4, 5, 'Ron said it was fantastic and he was right!!!'),
+(1515, 5, 1, 'I choked on a LEGO piece and nearly died.'),
+(1518, 1, 7, NULL),
+(1599, 4, 5, 'Ron didn''t lie!'),
+(1515, 6, 10, 'Fun for the whole family!');
 
 -- Helpfulness(reviewer, IID, observer, helpfulness)
 INSERT INTO Helpfulness VALUES
