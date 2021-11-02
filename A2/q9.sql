c3734918948367c624c43050738d7ab8114a9533
Jaakulan Subeethakumar <jaakulans@hotmail.com>
2021-11-01 23:51:20 -0400

    finished q9

--- A2/q9.sql
+++ A2/q9.sql
@@ -7,11 +7,20 @@ SET SEARCH_PATH TO Recommender;
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
-
+DROP VIEW IF EXISTS allOrders CASCADE;
 
 -- Define views for your intermediate steps here:
 
+---- earliest orders from yesterday from each user
+CREATE VIEW allOrders as 
+select pid, m.cid, d from (select cid, min(d) as mn from Purchase
+where d > TIMESTAMP 'yesterday'
+group by cid) t join Purchase m ON m.cid = t.cid and t.mn=m.d;
 
 -- Your SQL code that performs the necessary insertions goes here:
 
+INSERT INTO Item VALUES 
+((select MAX(iid) from Item)+1, 'Housewares', 'Company logo mug', 0.00);
+
+INSERT INTO LineItem VALUES
+((select pid from allOrders), (select max(iid) from Item), 1);
